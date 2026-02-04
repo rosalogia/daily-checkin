7c3eb321c09c9d31b789bd3450e8ffe5b42e1ca1
Ambika Eshwar <ambika@rosalogia.me>
2026-02-03 23:05:18 -0500

    Fix direnv issue in nix flake

--- flake.nix
+++ flake.nix
@@ -8,13 +8,13 @@
   };
 
   outputs = { self, nixpkgs, rust-overlay, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+    (flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        
+
         rustToolchain = pkgs.rust-bin.stable.latest.default.override {
           extensions = [ "rust-src" "rust-analyzer" ];
         };
@@ -84,8 +84,7 @@
           '';
         };
       }
-    )
-    {
+    )) // {
       nixosModules.daily-checkin = import ./nix/daily-checkin-module.nix;
     };
 }
